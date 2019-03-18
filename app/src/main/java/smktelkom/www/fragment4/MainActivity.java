88e1e0d4ae2925989657c28c463b0579f23018ec
wahyuutami <wahyu_utami_26rpl@student.smktelkom-mlg.sch.id>
2019-03-18 19:37:04 +0700

    finish

---
+++ app/src/main/java/smktelkom/www/fragment4/MainActivity.java
@@ -0,0 +1,46 @@
+package smktelkom.www.fragment4;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageButton buttonmaps;
+    private TextView Java;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
+        Java = (TextView)findViewById(R.id.java_text);
+        buttonmaps = (ImageButton)findViewById(R.id.buttonmaps);
+
+        buttonmaps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri geoLocation = Uri.parse("geo:0,0?q=-6.870563, 107.594126(SMK Telkom Malang)?z=23");
+                showMap(geoLocation);
+            }
+
+            private void showMap(Uri geoLocation) {
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW);
+                mapIntent.setData(geoLocation);
+                if (mapIntent.resolveActivity(getPackageManager())!= null)startActivity(mapIntent);
+            }
+        });
+        Java.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.udacity.com/learn/java"));
+                startActivity(intent);
+            }
+        });
+    }
+}
